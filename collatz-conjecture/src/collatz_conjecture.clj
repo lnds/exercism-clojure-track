7079be2abedd68f35918e8946f5f68072a50b972
Eduardo Díaz <ediaz@dcc8090.cl>
2021-06-21 13:35:25 -0400

    change if-let for when-let in collatz

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -5,5 +5,5 @@
     (neg? num) (throw (Exception. "no negative"))
     (zero? num) (throw (Exception. "no zero"))
     (== 1 num) 0
-    (zero? (mod num 2)) (if-let [a (collatz (quot num 2))] (inc a) nil)
-    :else (if-let [a (collatz (inc (* 3 num)))] (inc a) nil)))
+    (zero? (mod num 2)) (when-let [a (collatz (quot num 2))] (inc a))
+    :else (when-let [a (collatz (inc (* 3 num)))] (inc a))))
