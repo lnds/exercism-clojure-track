9a90964766e2174c46245ce9fad187251ba23e34
Eduardo Díaz <ediaz@dcc8090.cl>
2021-06-21 20:28:35 -0400

    solve phone number

--- phone-number/src/phone_number.clj
+++ phone-number/src/phone_number.clj
@@ -4,19 +4,18 @@
   (let 
     [num-seq (filter #(Character/isDigit %) num-string)
      first-digit (first num-seq)
-     len-num (count num-seq)]
+     len-num (count num-seq)
+     first-exchange (nth num-seq 3)]
     (cond
       (and (= 11 len-num) (= \1 first-digit)) (apply str (rest num-seq))
-      :else (apply str num-seq))))
+      (or (<= 11 len-num)  (>= 9 len-num)
+          (= \1 first-digit) (= \0 first-digit)
+          (= \0 first-exchange) (= \1 first-exchange)) "0000000000"
+      :else (apply str num-seq))))     
 
 (defn area-code [num-string] ;; <- arglist goes here
-  (let [num (number num-string)]
-    (->> 
-      (if (= 9 (count num)) num (rest num))
-      (take 3)
-      (apply str))))
+  (let [num (number num-string)] (->> num (take 3) (apply str))))
 
 
 (defn pretty-print [num-string] ;; <- arglist goes here
-  ;; your code goes here
-)
+  (let [num (number num-string)] (str "(" (subs num 0 3) ") " (subs num 3 6) "-" (subs num 6))))
