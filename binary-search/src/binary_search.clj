f58d0ce7a108a06d14096a8d203129e7783daa9a
Eduardo Díaz <ediaz@dcc8090.cl>
2021-06-26 14:02:51 -0400

    solve binary search

--- binary-search/src/binary_search.clj
+++ binary-search/src/binary_search.clj
@@ -1,9 +1,14 @@
 (ns binary-search)
 
-(defn search-for [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn middle [list] 
+   (int (Math/floor (/ (count list) 2))))
 
-(defn middle [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn search-for [value list] 
+  (case (count list) 
+    0 (throw (Exception. "not found"))
+    1 (if (= value (first list)) 0 (throw (Exception. "not found")))
+    (let [m (middle list) pivot (nth list m)]
+      (cond 
+        (= pivot value) m
+        (> pivot value) (search-for value (take m list))
+        :else (+ m (search-for value (drop m list)))))))
