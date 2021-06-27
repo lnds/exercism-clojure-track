0acdfcb60ccc7330d2c4d468b164e0d467c1a80d
Eduardo Díaz <ediaz@dcc8090.cl>
2021-06-26 20:59:29 -0400

    solve triangle

---
+++ triangle/src/triangle.clj
@@ -0,0 +1,13 @@
+(ns triangle)
+
+(defn is-valid? [a b c]
+  (and (> (+ a b) c) (> (+ a c) b) (> (+ b c) a)))
+
+(defn equilateral? [a b c] 
+  (and (is-valid? a b c) (= a b c)) )
+
+(defn isosceles? [a b c] 
+  (and (is-valid? a b c) (or (= a b) (= a c) (= b c))))
+
+(defn scalene? [a b c] 
+  (and (is-valid? a b c) (not= a b) (not= b c)))
