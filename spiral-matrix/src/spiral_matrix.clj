049de3e29cb5034af86db4c1a82279473a038c59
Eduardo Díaz <ediaz@dcc8090.cl>
2021-06-27 01:32:28 -0400

    solve spiral matrix

---
+++ spiral-matrix/src/spiral_matrix.clj
@@ -0,0 +1,16 @@
+(ns spiral-matrix)
+
+(defn- cell [n row col]
+  (cond
+    (= row 0) (inc col)
+    (= col 0) (- (* 4 n) 3 row)
+    (= row (dec n)) (- (* 3 n) 2 col) 
+    (= col (dec n)) (+ n row)
+    :else (+ (* 4 (dec n))     
+             (cell (- n 2) (dec row) (dec col)))))
+
+(defn spiral [n]
+  (->> 
+    (for [a (range n) b (range n)] [a b])
+    (map #(apply (partial cell n) %))
+    (partition n)))exerism
